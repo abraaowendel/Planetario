893c42e074467c5cf46300991f3f5b914bdfb859
Abraao Wendel <abraao.dev@gmail.com>
2022-04-08 19:57:05 -0300

    layout pronto

---
+++ src/assets/js/JSON.js
@@ -0,0 +1,22 @@
+const data = [
+    {   
+        id: 1,
+        name: a,
+        overView:{
+            urls: a,
+        },
+        internalStructure:{
+            urls: a,
+        },
+        surfaceGeology:{
+            urls: a,
+        },
+        generalData:{
+            rotationTime: a,
+            revolutionTime: a,
+            radius: a,
+            averageTemp: a,
+        }
+    },
+]
+   
---
+++ src/assets/js/menu.js
@@ -0,0 +1,29 @@
+const menuToogle = document.querySelector('.hamburguer')
+const menu = document.querySelector('.menu')
+
+menuToogle.addEventListener('click', () =>{
+    menuToogle.classList.toggle('active')
+    menu.classList.toggle('active')
+})
+
+document.querySelectorAll('.menu li').forEach((item) => item.addEventListener("click" , (a) =>{
+    menuToogle.classList.remove('active')
+    menu.classList.remove('active')
+
+    const ativos = document.querySelectorAll('.menu li.active')
+    const remover = () => ativos.forEach((item) => item.classList.remove('active'));
+    const adicionar = () => a.currentTarget.classList.add('active')
+
+    remover();
+    adicionar();
+}))
+
+
+document.querySelectorAll('.side-right--types .box')
+.forEach((item) => item.addEventListener('click', (e) => {
+    const ativos = document.querySelectorAll('.box.active')
+    const remover = () => ativos.forEach((item) => item.classList.remove('active'))
+    const adicionar = () => e.currentTarget.classList.add('active')
+    remover();
+    adicionar();
+}))
